96bec61f1f9d5ea95c9d168e8531dc81688a2393
firatinlkc <firatinalkac2@gmail.com>
2021-01-20 22:04:16 +0300

    contact form

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
+
+import {FormContainer} from "./Components/"
+
+
+function App() {
+  return (
+    <Container>
+      <FormContainer />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Components/FormBody.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {Button, Card, Accordion, InputGroup} from 'react-bootstrap';
+
+function FormBody(props){
+
+
+  return(
+    <div>
+      {props.datas.map((data, index) => {
+        return (
+          <div>
+            <InputGroup className="d-block">
+              <Accordion className="mt-5 pt-5" defaultActiveKey="0">
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                      <p>{data.name}</p>
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                    <p><b>Name:</b>{data.name}</p>
+                    <p><b>Surname:</b>{data.surname}</p>
+                    <p><b>E-mail:</b>{data.email}</p>
+                    <p><b>Title:</b>{data.title}</p>
+                    <p><b>Note:</b>{data.note}</p>
+                    <div >
+                      <Button
+                          className="d-inline-block m-5"
+                          type="submit"
+                          className="btn btn-danger"
+                          onClick={() => props.deletePost(index)}
+                      >
+                        <i class="fas fa-user-minus"></i>
+                      </Button>
+                      <Button
+                        type="submit"
+                        className="btn btn-success"
+                        onClick={() =>
+                          props.editPost({
+                            id: index,
+                            name: data.name,
+                            surname: data.surname,
+                            email: data.email,
+                            title: data.title,
+                            note: data.note,
+                            })
+                          }
+                        >
+                          <i class="fas fa-user-edit"></i>
+                        </Button>
+                      </div>  
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+                <Card>               
+                  <Accordion.Collapse eventKey="1">
+                    <Card.Body>Hello! I'm another body</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </Accordion>
+            </InputGroup>         
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
+export default FormBody;
+
+
+
+
+    
+
